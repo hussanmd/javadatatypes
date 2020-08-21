9a2ee435d5e86a783cb3de2711ceda6542036509
hussanmd <48239261+hussanmd@users.noreply.github.com>
2020-08-21 11:19:35 -0400

    This is my first java class

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to JAVA");
+		System.out.println("My name is "+ args[0] +" "+ args[1]);
+		
+	}
+
+}
