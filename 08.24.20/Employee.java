33d4ed7c74052809acd9eb94fc35290aa9a62a1f
hussanmd <48239261+hussanmd@users.noreply.github.com>
2020-08-25 09:46:10 -0400

    Student files are added.

---
+++ 08.24.20/Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+public static void main(String[] numbers){
+
+System.out.println("First number: " + numbers[0]+" "+ "Second number:" + numbers[1]+" "+"Third number" +numbers[2]);
+
+}
+
+}
